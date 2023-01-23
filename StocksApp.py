1423dd4306cf123a9d58a983e99ec549ccc7f97b
Rohit <rohit2k02@gmail.com>
2023-01-23 18:47:28 +0530

    Update StocksApp.py

--- StocksApp.py
+++ StocksApp.py
@@ -3,10 +3,8 @@ import streamlit as st
 import datetime
 import pandas as pd
 
-
-st.set_page_config(page_title="Stocks",page_icon="📈")
+st.set_page_config(page_title="Stocks", page_icon="📈")
 st.title('Welcome to Stock Comparer!')
-
 
 with st.container():
     st.header('How to Use ?')
@@ -38,8 +36,8 @@ with st.container():
         tickername3 = st.selectbox('Select Stock 3',
                                    ['GOOGL', 'AAPL', 'AMZN', 'TSLA', 'META', 'MSFT', 'NVDA', 'AMD', 'INTC'])
 
-    back = st.select_slider('Select History Length for Graph (in years)', options=[1,2,3,4,5,6,7,8,9,10,11,12], value=6)
-
+    back = st.select_slider('Select History Length for Graph (in years)',
+                            options=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], value=6)
 
 with st.container():
     today = datetime.date.today()
@@ -58,8 +56,8 @@ with st.container():
         todaytickerDF1 = tickerdata1.history(period='1min')
     else:
         pass
-    todayprice1 = round(todaytickerDF1['Close'][0],2)
-    todaytickerDF10 = tickerdata1.history(period='1d',start=f"{y}-{m}-{d-2}", end=f"{y}-{m}-{d-1}")
+    todayprice1 = round(todaytickerDF1['Close'][0], 2)
+    todaytickerDF10 = tickerdata1.history(period='1d', start=f"{y}-{m}-{d - 2}", end=f"{y}-{m}-{d - 1}")
     try:
         todayprice10 = round(todaytickerDF10['Close'][0], 2)
     except IndexError:
@@ -67,7 +65,6 @@ with st.container():
     else:
         todayprice10 = round(todaytickerDF10['Close'][0], 2)
     tickerDF1 = tickerdata1.history(period='1d', start=f"{y - back}-{m}-{d}", end=f"{y}-{m}-{d}")
-
 
     tickerdata2 = yf.Ticker(tickername2)
     todaytickerDF2 = tickerdata2.history(period='1min')
@@ -80,8 +77,8 @@ with st.container():
         todaytickerDF2 = tickerdata2.history(period='1min')
     else:
         pass
-    todayprice2 = round(todaytickerDF2['Close'][0],2)
-    todaytickerDF20 = tickerdata2.history(period='1d',start=f"{y}-{m}-{d-2}", end=f"{y}-{m}-{d-1}")
+    todayprice2 = round(todaytickerDF2['Close'][0], 2)
+    todaytickerDF20 = tickerdata2.history(period='1d', start=f"{y}-{m}-{d - 2}", end=f"{y}-{m}-{d - 1}")
     try:
         todayprice20 = round(todaytickerDF20['Close'][0], 2)
     except IndexError:
@@ -89,7 +86,6 @@ with st.container():
     else:
         todayprice20 = round(todaytickerDF20['Close'][0], 2)
     tickerDF2 = tickerdata2.history(period='1d', start=f"{y - back}-{m}-{d}", end=f"{y}-{m}-{d}")
-
 
     tickerdata3 = yf.Ticker(tickername3)
     todaytickerDF3 = tickerdata3.history(period='1min')
@@ -102,8 +98,8 @@ with st.container():
         todaytickerDF3 = tickerdata3.history(period='1min')
     else:
         pass
-    todayprice3 = round(todaytickerDF3['Close'][0],2)
-    todaytickerDF30 = tickerdata3.history(period='1d',start=f"{y}-{m}-{d-2}", end=f"{y}-{m}-{d-1}")
+    todayprice3 = round(todaytickerDF3['Close'][0], 2)
+    todaytickerDF30 = tickerdata3.history(period='1d', start=f"{y}-{m}-{d - 2}", end=f"{y}-{m}-{d - 1}")
     try:
         todayprice30 = round(todaytickerDF30['Close'][0], 2)
     except IndexError:
@@ -112,28 +108,22 @@ with st.container():
         todayprice30 = round(todaytickerDF30['Close'][0], 2)
     tickerDF3 = tickerdata3.history(period='1d', start=f"{y - back}-{m}-{d}", end=f"{y}-{m}-{d}")
 
-
     data = [tickerDF1.Close, tickerDF2.Close, tickerDF3.Close]
     data = pd.DataFrame(data)
     data = data.transpose()
     data.columns = [f'{tickername1}', f'{tickername2} ', f'{tickername3}  ']
 
-
 col1, col2, col3 = st.columns(3)
 with col1:
-    st.metric(label=f'{tickername1}', value=f"{todayprice1} USD", delta=f'{round(todayprice1-todayprice10,2)} USD')
+    st.metric(label=f'{tickername1}', value=f"{todayprice1} USD", delta=f'{round(todayprice1 - todayprice10, 2)} USD')
 with col2:
-    st.metric(label=f'{tickername2}', value=f"{todayprice2} USD", delta=f'{round(todayprice2-todayprice20,2)} USD')
+    st.metric(label=f'{tickername2}', value=f"{todayprice2} USD", delta=f'{round(todayprice2 - todayprice20, 2)} USD')
 with col3:
-    st.metric(label=f'{tickername3}', value=f"{todayprice3} USD", delta=f'{round(todayprice3-todayprice30,2)} USD')
+    st.metric(label=f'{tickername3}', value=f"{todayprice3} USD", delta=f'{round(todayprice3 - todayprice30, 2)} USD')
 
 with st.container():
     st.line_chart(data=data)
 
-
 with st.container():
     st.caption("Made by Rohit")
     st.markdown("[![Foo](https://cdn-icons-png.flaticon.com/24/25/25231.png)](https://github.com/ItsNotRohit02)")
-
-
-
